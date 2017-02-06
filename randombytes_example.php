6d19d017ff86db0909e768ca72eafacc66ea2a6f
Tinram <Tinram@users.noreply.github.com>
2017-02-06 22:02:14 +0000

    add PHP 7 random_bytes() support

--- randombytes_example.php
+++ randombytes_example.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 /**
 	* Example web server usage of RandomBytes class.
@@ -6,7 +6,7 @@
 */
 
 ###################################################
-mb_internal_encoding('UTF-8');	
+mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 header('Content-Type: text/html; charset=utf-8');
 ###################################################
@@ -17,7 +17,7 @@ use CopySense\RandomBytes\RandomBytes;
 ###################################################
 
 
-$aRandGenMethods = ['mcrypt', 'openssl', 'urandom'];
+$aRandGenMethods = ['openssl', 'urandom', 'random_bytes', 'mcrypt'];
 
 $bSubmitted = (isset($_POST['submit_check'])) ? TRUE : FALSE;
 
@@ -54,12 +54,12 @@ function generateTable(array $aData) {
 
 			h1 {margin:0 0 20px 0; font-size:1.1em;}
 
-			div#frbcont {width:360px; margin:0 0 60px 0;}
+			div#frbcont {width:376px; margin:0 0 60px 0;}
 			form#frb label {margin:0 5px 0 0;}
 			form#frb input, form#frb select {border-radius:5px; background:#f2f9f9; padding:2px; border:1px solid #c8e4ff; background:linear-gradient(to top, #ebf8ff, #ffffff 80%);}
 			form#frb select {margin:0 12px 0 0;}
 			form#frb select#chars {width:50px;}
-			form#frb select#genmethod {width:85px;}
+			form#frb select#genmethod {width:120px;}
 			form#frb input[type='submit'] {width:80px; height:24px; color:#333; background:#f7ffff; border:0; font-weight:bold; text-align:center; margin-top:10px; padding-bottom:3px; height:25px; background:linear-gradient(to bottom, #f7ffff, #c8e4ff 100%);}
 			form#frb input[type='submit']:hover {background:#39f; color:#fff; cursor:pointer;}
 
